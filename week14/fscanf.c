4820c2d1ce6409c550fcf7064412ac7d43736296
ththfql <neozang12345@gmail.com>
2025-12-09 16:29:54 +0900

    week14

---
+++ week14/fscanf.c
@@ -0,0 +1,27 @@
+﻿// fscanf.c
+
+#include <stdio.h>
+
+int main(void) {
+	int temp;
+	FILE* p_file = fopen("nums.txt", "rt");
+
+	if (NULL != p_file) {
+		printf("파일을 열었습니다!");
+
+		while (EOF != fscanf_s(p_file, "%d", &temp)) {
+			// 파일에서 읽은 숫자 값을 화면에 출력함
+			if (temp % 3 == 0) {
+				printf("\n");
+			}
+			printf("%d ", temp);
+		}
+		printf("\n");
+	}
+
+	printf("파일의 내용을 읽었습니다.\n");
+	fclose(p_file);
+	printf("파일을 닫았습니다.");
+
+	return 0;
+}
---
+++ week14/my_math.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+// my_math.h
+
+int Sum(int a, int b, void(*pa)(int*), void(*pb)(int*));
+int Sub(int a, int b, void(*pa)(int*), void(*pb)(int*));
+int Mul(int a, int b, void(*pa)(int*), void(*pb)(int*));
+int Div(int a, int b, void(*pa)(int*), void(*pb)(int*));
+int Rem(int a, int b, void(*pa)(int*), void(*pb)(int*));
+
+// my_math.lib
+
+int Sum(int a, int b, void(*pa)(int*), void(*pb)(int*)) {
+	if (NULL != pa) {
+		(*pa)(&a); // => MyAbs(a)
+	}
+	if (NULL != pb) {
+		(*pb)(&b); // => MyAbs(b)
+	}
+	return a + b;
+}
+
+
+int Sub(int a, int b, void(*pa)(int*), void(*pb)(int*)) {
+	if (NULL != pa) {
+		(*pa)(&a); // => MyAbs(a)
+	}
+	if (NULL != pb) {
+		(*pb)(&b); // => MyAbs(b)
+	}
+	return a - b;
+}
+
+
+int Mul(int a, int b, void(*pa)(int*), void(*pb)(int*)) {
+	if (NULL != pa) {
+		(*pa)(&a); // => MyAbs(a)
+	}
+	if (NULL != pb) {
+		(*pb)(&b); // => MyAbs(b)
+	}
+	return a * b;
+}
+
+
+int Div(int a, int b, void(*pa)(int*), void(*pb)(int*)) {
+	if (NULL != pa) {
+		(*pa)(&a); // => MyAbs(a)
+	}
+	if (NULL != pb) {
+		(*pb)(&b); // => MyAbs(b)
+	}
+	return a / b;
+}
+
+
+int Rem(int a, int b, void(*pa)(int*), void(*pb)(int*)) {
+	if (NULL != pa) {
+		(*pa)(&a); // => MyAbs(a)
+	}
+	if (NULL != pb) {
+		(*pb)(&b); // => MyAbs(b)
+	}
+	return a % b;
+}
